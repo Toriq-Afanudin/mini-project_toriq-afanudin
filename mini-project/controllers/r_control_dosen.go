6a9370d6943e97a28dab557532b31082c7d4b336
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-05-08 20:42:06 +0700

    update

--- mini-project/controllers/r_control_dosen.go
+++ mini-project/controllers/r_control_dosen.go
@@ -4,8 +4,6 @@ import (
 	"mini_project/models"
 	"net/http"
 
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
@@ -15,8 +13,5 @@ func Dosen_pengampu_tampil(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 	var Daftar_dosen []models.Dosen_pengampu
 	db.Find(&Daftar_dosen)
-	for i := 0; i < 15; i++ {
-		fmt.Println(Daftar_dosen[i])
-	}
 	c.JSON(http.StatusOK, gin.H{"data": Daftar_dosen})
 }
