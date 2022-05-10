832c66a58884e8dcfca8fc006de9940ca9842c60
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-05-10 20:34:36 +0700

    menambahkan akumulasi teori ring kelas B

--- mini-project/controllers/r_control_akumulasi.go
+++ mini-project/controllers/r_control_akumulasi.go
@@ -4,8 +4,6 @@ import (
 	"mini_project/models"
 	"net/http"
 
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 )
@@ -31,35 +29,71 @@ func Akumulasi_kelas_teori_ring_B(c *gin.Context) {
 	var d models.Daftar_mahasiswa
 	var Id_mahasiswa []int
 	var Kehadiran []int
-	var Nama_mahasiswa []string
 
-	for i := 0; i < 21; i++ {
+	for i := 0; i < len(Hadir); i++ {
 		j = Hadir[i]
 		Id_mahasiswa = append(Id_mahasiswa, j.Id_mahasiswa)
 		Kehadiran = append(Kehadiran, j.Kehadiran)
 	}
 
-	for i := 0; i < 7; i++ {
+	var Nama_mahasiswa []string
+	for i := 0; i < len(Mahasiswa[0:7]); i++ {
 		d = Mahasiswa[i]
 		Nama_mahasiswa = append(Nama_mahasiswa, d.Nama)
 	}
 
-	fmt.Println(Id_mahasiswa)
-	fmt.Println(Kehadiran)
-	fmt.Println(Nama_mahasiswa)
-
 	var Unix_id_mahasiswa []int
-	for i := 1; 1 <= 7; i++ {
-		Unix_id_mahasiswa = append(Unix_id_mahasiswa, i)
+	Unix_id_mahasiswa = append(Unix_id_mahasiswa, Id_mahasiswa[0])
+	if Id_mahasiswa[1] != Id_mahasiswa[0] {
+		Unix_id_mahasiswa = append(Unix_id_mahasiswa, Id_mahasiswa[1])
 	}
 
-	var Jumlah int
-	for i := 0; i < 21; i++ {
-		if Unix_id_mahasiswa[0] == Id_mahasiswa[i] {
-			Jumlah++
+	for i := 2; i < len(Hadir); i++ {
+		var Jumlah int
+		for m := i - 1; m >= 0; m-- {
+			if Id_mahasiswa[i] == Id_mahasiswa[m] {
+				Jumlah++
+			}
+		}
+		if Jumlah == 0 {
+			Unix_id_mahasiswa = append(Unix_id_mahasiswa, Id_mahasiswa[i])
 		}
 	}
-	fmt.Println(Jumlah)
 
-	c.JSON(http.StatusOK, gin.H{"data": "Akumulasi Berhasil Dilakukan"})
+	type Akumulasi struct {
+		Nama_mahasiswa     string
+		Jumlah_pertemuan   int
+		Jumlah_hadir       int
+		Jumlah_tidak_hadir int
+	}
+
+	var akumulasi Akumulasi
+	var Akm []interface{}
+
+	for i := 0; i < len(Unix_id_mahasiswa); i++ {
+		var Jumlah_hadir int
+		var Jumlah_pertemuan int
+		var Jumlah_tidak_hadir int
+		Jumlah_hadir = 0
+		for j := 0; j < len(Id_mahasiswa); j++ {
+			if Unix_id_mahasiswa[i] == Id_mahasiswa[j] {
+				Jumlah_hadir = Jumlah_hadir + Kehadiran[j]
+				Jumlah_pertemuan++
+			}
+			Jumlah_tidak_hadir = Jumlah_pertemuan - Jumlah_hadir
+		}
+		akumulasi.Nama_mahasiswa = Nama_mahasiswa[i]
+		akumulasi.Jumlah_hadir = Jumlah_hadir
+		akumulasi.Jumlah_pertemuan = Jumlah_pertemuan
+		akumulasi.Jumlah_tidak_hadir = Jumlah_tidak_hadir
+		Akm = append(Akm, akumulasi)
+	}
+
+	// fmt.Println(Akm)
+	// fmt.Println(Unix_id_mahasiswa)
+	// fmt.Println(Id_mahasiswa)
+	// fmt.Println(Kehadiran)
+	// fmt.Println(Nama_mahasiswa)
+
+	c.JSON(http.StatusOK, gin.H{"Akumulasi Teori Ring Kelas B": Akm})
 }
