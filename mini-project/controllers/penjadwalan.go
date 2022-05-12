a5b099ac67e817eddbd946af6f5b55bd167bbae3
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-05-12 17:30:18 +0700

    update

--- mini-project/controllers/penjadwalan.go
+++ mini-project/controllers/penjadwalan.go
@@ -19,7 +19,7 @@ func Get_penjadwalan(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 	var Jadwal []models.Penjadwalan
 	db.Find(&Jadwal)
-	c.JSON(http.StatusOK, gin.H{"data": Jadwal})
+	c.JSON(http.StatusOK, gin.H{"JADWAL": Jadwal})
 }
 
 func Post_penjadwalan(c *gin.Context) {
@@ -28,7 +28,7 @@ func Post_penjadwalan(c *gin.Context) {
 	//MEMASTIKAN INPUTAN DALAM BENTUK JSON
 	var Input Input_penjadwalan
 	if err := c.ShouldBindJSON(&Input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"ERROE": "INPU TIDAK DALAM BENTUK JSON"})
 		return
 	}
 
@@ -55,22 +55,13 @@ func Post_penjadwalan(c *gin.Context) {
 	//MEMASTIKAN MATAKULIAH DAN DOSEN PENGAMPU YANG DI INPUT ADA DALAM TABEL KELAS
 	var validasi1 int
 	for i := 0; i < len(matakuliah); i++ {
-		if Input.Matakuliah == matakuliah[i] {
+		if (Input.Matakuliah == matakuliah[i]) && (Input.Dosen_pengampu_tanpa_gelar == dosen[i]) {
 			validasi1++
 		}
 	}
 	if validasi1 == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "Matakuliah yang Anda input tidak ditemukan"})
-
-	}
-	var validasi2 int
-	for i := 0; i < len(dosen); i++ {
-		if Input.Dosen_pengampu_tanpa_gelar == dosen[i] {
-			validasi2++
-		}
-	}
-	if validasi2 == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "Nama Dosen yang Anda input tidak ditemukan"})
+		message := "MATAKULIAH: '" + Input.Matakuliah + "' TIDAK DIAMPU OLEH DOSEN '" + Input.Dosen_pengampu_tanpa_gelar + "'"
+		c.JSON(http.StatusBadRequest, gin.H{"GAGAL MENAMBAHKAN JADWAL": message})
 
 	}
 
@@ -85,14 +76,14 @@ func Post_penjadwalan(c *gin.Context) {
 	}
 
 	//MEMASTIKAN JAM PERKULIAHAN ADA
-	var validasi3 int
+	var validasi2 int
 	for i := 0; i < len(jam_kuliah); i++ {
 		if Input.Jam_perkuliahan == jam_kuliah[i] {
-			validasi3++
+			validasi2++
 		}
 	}
-	if validasi3 == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "Jam perkuliahan yang Anda input tidak ditemukan"})
+	if validasi2 == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"GAGAL MENAMBAHKAN JADWAL": "JAM PERKULIAHAN: '" + Input.Jam_perkuliahan + "' YANG ANDA INPUT TIDAK DI TEMUKAN"})
 
 	}
 
@@ -109,23 +100,37 @@ func Post_penjadwalan(c *gin.Context) {
 	}
 
 	//MEMASTIKAN TANGGAL DAN JAM BELUM DIGUNAKAN
-	var validasi4 int
+	var validasi3 int
 	if tanggal != nil {
 		for i := len(tanggal) - 1; i >= 0; i-- {
 			if (Input.Tanggal_perkuliahan+"T00:00:00+07:00" == tanggal[i]) && (Input.Jam_perkuliahan == jam[i]) {
-				validasi4++
+				validasi3++
 			}
 		}
-		if validasi4 != 0 {
-			c.JSON(http.StatusBadRequest, gin.H{"Error": "Tanggal dan jam perkuliahan sudah digunakan"})
+		if validasi3 != 0 {
+			message := "TANGGAL DAN JADWAL TELAH DIGUNAKAN"
+			c.JSON(http.StatusBadRequest, gin.H{"GAGAL MENAMBAHKAN JADWAL": message})
 		}
 	}
 
 	//MEMASTIKAN JUMLAH PERTEMUAN TIDAK LEBIH DARI 7
 
 	//JIKA MATAKULIAH DAN DOSEN DAN JAM PERKULIAHAN ADA MAKA DATA AKAN DI INPUTKAN
-	if (validasi1 != 0) && (validasi2 != 0) && (validasi3 != 0) && (validasi4 == 0) {
+	if (validasi1 != 0) && (validasi2 != 0) && (validasi3 == 0) {
 		db.Create(&input)
-		c.JSON(http.StatusOK, gin.H{"Data yang telah di tambahkans": input})
+		type berhasil struct {
+			Matakuliah          string
+			Dosen_pengampu      string
+			Tanggal_perkuliahan string
+			Jam_perkuliahan     string
+			Keterangan          string
+		}
+		var a berhasil
+		a.Matakuliah = Input.Matakuliah
+		a.Dosen_pengampu = Input.Dosen_pengampu_tanpa_gelar
+		a.Tanggal_perkuliahan = Input.Tanggal_perkuliahan
+		a.Jam_perkuliahan = Input.Jam_perkuliahan
+		a.Keterangan = "BERHASIL"
+		c.JSON(http.StatusOK, gin.H{"PENJADWALAN BERHASIL DITAMBAHKAN": a})
 	}
 }
