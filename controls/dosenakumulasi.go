ccf74d17d8ff1e283f7de83175368a1a38f67a8b
Toriq Afanudin <toriq1900006105@webmail.uad.ac.id>
2022-05-16 18:36:57 +0700

    edit akumulasi

--- controls/dosenakumulasi.go
+++ controls/dosenakumulasi.go
@@ -12,28 +12,8 @@ func DosenAkumulasi(c *gin.Context) {
 
 	var column tabels.Dosen
 	db.Where("nip = ?", c.Param("nip")).Find(&column)
-	type matakuliah struct {
-		Matakuliah string `json:"matakuliah"`
-	}
-	var m matakuliah
-	if err := c.ShouldBindJSON(&m); err != nil {
-		c.JSON(400, gin.H{
-			"status":  "error",
-			"message": "input tidak dalam bentuk json",
-		})
-		return
-	}
-	var column2 tabels.Krs
-	db.Where("dosen = ?", column.Gelar).Where("matakuliah = ?", m.Matakuliah).Find(&column2)
-	if m.Matakuliah != column2.Matakuliah {
-		c.JSON(400, gin.H{
-			"status":  "error",
-			"message": "saudara tidak mengampu matakuliah ini",
-		})
-		return
-	}
 	var tabel []tabels.Akumulasi
-	db.Where("matakuliah = ?", column2.Matakuliah).Find(&tabel)
+	db.Where("dosen = ?", column.Gelar).Find(&tabel)
 
 	c.JSON(200, gin.H{
 		"status": "berhasil",
--- controls/mahasiswaakumulasi.go
+++ controls/mahasiswaakumulasi.go
@@ -12,26 +12,6 @@ func MahasiswaAkumulasi(c *gin.Context) {
 
 	var column tabels.Mahasiswa
 	db.Where("nim = ?", c.Param("nim")).Find(&column)
-	type matakuliah struct {
-		Matakuliah string `json:"matakuliah"`
-	}
-	var m matakuliah
-	if err := c.ShouldBindJSON(&m); err != nil {
-		c.JSON(400, gin.H{
-			"status":  "error",
-			"message": "input tidak dalam bentuk json",
-		})
-		return
-	}
-	var column2 tabels.Krs
-	db.Where("nama = ?", column.Nama).Where("matakuliah = ?", m.Matakuliah).Find(&column2)
-	if m.Matakuliah != column2.Matakuliah {
-		c.JSON(400, gin.H{
-			"status":  "error",
-			"message": "anda tidak mengambil matakuliah ini",
-		})
-		return
-	}
 	var tabel []tabels.Akumulasi
 	db.Where("nama = ?", column.Nama).Find(&tabel)
 
